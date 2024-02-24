3430c074948aa3af47d9edcffdc186afbf8a7e1d
salim aliyu musawa <salemaliyu1992@gmail.co>
2024-02-24 11:42:23 -0800

    count each

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,7 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route("/status")
@@ -11,3 +12,18 @@ def status():
 
     var = {"status": "ok"}
     return jsonify(var)
+
+
+@app_views.route("/stats")
+def stats():
+    """ Thin method retrieves the number of each objects by type. """
+
+    dic = {
+            "amenities": storage.count("Amenity"),
+            "cities": storage.count("City"),
+            "places": storage.count("Place"),
+            "reviews": storage.count("Review"),
+            "states": storage.count("State"),
+            "users": storage.count("User")
+            }
+    return jsonify(dic)
