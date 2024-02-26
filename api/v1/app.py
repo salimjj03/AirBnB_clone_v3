f4fdd32017fb7729ffbf028b21e086bf5f887f60
salim aliyu musawa <salemaliyu1992@gmail.co>
2024-02-26 07:09:41 -0800

    cors

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,10 +5,12 @@ from flask import Flask, make_response, jsonify
 from os import getenv
 from models import storage
 from api.v1.views import app_views
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+CORS(app, resources={r"/api/*": {"origins": "http://0.0.0.0"}})
 
 
 @app.teardown_appcontext
